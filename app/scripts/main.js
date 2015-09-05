df4944ca378072820cec373aeb5f2d99a1012703
Kyle Permuy <kyle@varninja.com>
2015-09-05 00:22:10 -0400

    Adds Backbone skeleton

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global PaprojectTeacher, $*/
+
+
+window.PaprojectTeacher = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    PaprojectTeacher.init();
+});
