8880b6ce9617f1ae00e622ffee5b55d639ed1619
Kyle Permuy <kyle@varninja.com>
2015-09-05 01:20:29 -0400

    Adds requirejs and parse

--- README.md
+++
@@ -1,3 +0,0 @@
-# Some random project.
-
-Project worked on at PennApps XII!
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,42 @@
-/*global PaprojectTeacher, $*/
+/*global require*/
+'use strict';
 
+require.config({
+   shim: {
+       bootstrap: {
+           deps: [
+               'jquery'
+           ],
+           exports: 'jquery'
+       },
+       'parse-js-sdk': {
+           deps:[
+           'jquery'
+           ],
+           exports:'Parse'
+       }
+   },
+   paths: {
+       jquery: '../bower_components/jquery/dist/jquery',
+       backbone: '../bower_components/backbone/backbone',
+       underscore: '../bower_components/lodash/dist/lodash',
+       bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
+       'bootstrap-sass-official': '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
+       chai: '../bower_components/chai/chai',
+       lodash: '../bower_components/lodash/dist/lodash.compat',
+       mocha: '../bower_components/mocha/mocha',
+       modernizr: '../bower_components/modernizr/modernizr',
+       'parse-js-sdk': '../bower_components/parse-js-sdk/lib/parse',
+       requirejs: '../bower_components/requirejs/require',
+       'requirejs-text': '../bower_components/requirejs-text/text'
+   },
+   packages: [
 
-window.PaprojectTeacher = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
-};
+   ]
+});
 
-$(document).ready(function () {
-    'use strict';
-    PaprojectTeacher.init();
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
 });
